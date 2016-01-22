292f44d94fdc0806b0714650cfb37ea8caf6dd42
David Gageot <david@gageot.net>
2016-01-22 20:55:34 +0100

    corrige le test
    Signed-off-by: David Gageot <david@gageot.net>

--- marcel_test.go
+++ marcel_test.go
@@ -1,6 +1,18 @@
 package main
 
+import "os"
+
 func ExampleHello() {
+	os.Args = []string{"marcel", "attache", "--aide"}
+
 	main()
-	// Output: Usage: docker [OPTIONS] COMMAND [arg...]
+
+	// Output:
+	// Utilisation:	marcel attache [OPTIONS] CONTAINER
+	//
+	// Attach to a running container
+	//
+	//   --help=false        Print usage
+	//   --no-stdin=false    Do not attache STDIN
+	//   --sig-proxy=true    Proxy all received signals to the process
 }
